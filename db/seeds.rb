599df7fd29f00cfef89fac6edba78d6ba30230ee
Andre Pires <andrecabralpires@gmail.com>
2018-11-19 10:04:40 +0200

    movie" "

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+Movie.destroy_all
+if User.find_by_email('quentin.tarantino@gmail.com')
+User.find_by_email('quentin.tarantino@gmail.com').destroy
+end
+
+user = User.create!(email: 'quentin.tarantino@gmail.com', password: '123456')
+
+url = 'https://raw.githubusercontent.com/rodloboz/lw-fullstack/master/topflix/data/movies.json'
+
+movies = JSON.parse(open(url).read)
+
+movies.take(20).each do |movie|
+  Movie.create!(
+    user: user,
+    title: movie["title"],
+    rank: movie["rank"].to_i
+  )
+end
+
+puts "Created #{Movie.count} movies!"
